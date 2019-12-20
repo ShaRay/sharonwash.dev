032b50a5f3c1c51ba929d84e091b49bd82f62460
ShaRay <sharay22.22@gmail.com>
2019-12-20 10:18:58 -0500

    added tailwind css

--- gridsome.config.js
+++ gridsome.config.js
@@ -15,7 +15,8 @@ module.exports = {
         path: "content/posts/**/*.md",
         typeName: "Post",
         route: "/blog/:slug"
-      }
+      },
+      use: "gridsome-plugin-tailwindcss"
     }
   ]
 };
